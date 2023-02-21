6b53751bb7ffcc25c27b01525b7a096d9ea14da4
André Sernajoto <andre.sernajoto@gmail.com>
2023-02-20 21:37:33 -0300

    adding validation with angular

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validate.form';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +11,32 @@ export class LoginComponent implements OnInit {
   type: string = 'password'
   isText: boolean = false
   eyeIcon: string = 'bi-eye-slash'
-  
-  constructor() { }
+  loginForm!: FormGroup
 
-  ngOnInit(): void { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   hideShowPass() {
     this.isText = !this.isText
     this.isText ? this.eyeIcon = 'bi-eye-fill' : this.eyeIcon = 'bi-eye-slash'
     this.isText ? this.type = 'text' : this.type = 'password'
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // send data to db
+      console.log(this.loginForm.value)
+
+    } else {
+      // throw error and show toastr
+      ValidateForm.validateAllFormFields(this.loginForm)
+      alert('Your form is invalid!')
+    }
   }
 }
--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validate.form';
 
 @Component({
   selector: 'app-signup',
@@ -9,14 +11,34 @@ export class SignupComponent  implements OnInit {
   type: string = 'password'
   isText: boolean = false
   eyeIcon: string = 'bi-eye-slash'
+  signUpForm!: FormGroup
   
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
 
   hideShowPass() {
     this.isText = !this.isText
     this.isText ? this.eyeIcon = 'bi-eye-fill' : this.eyeIcon = 'bi-eye-slash'
     this.isText ? this.type = 'text' : this.type = 'password'
+  }
+
+  onSignUp() {
+    if (this.signUpForm.valid) {
+      // send data to db
+      console.log(this.signUpForm.value)
+    } else {
+      // throw error and show toastr
+      ValidateForm.validateAllFormFields(this.signUpForm)
+      alert('Your form is invalid!')
+    }
   }
 }
