41a5957b92fb118bb8ced629d3a6c4360a1ff029
André Sernajoto <andre.sernajoto@gmail.com>
2023-02-22 02:14:17 -0300

    encrypting password and validating user info

--- AngularAuthAPI/Controllers/UserController.cs
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -1,8 +1,11 @@
+using System.Text.RegularExpressions;
+using System.Text;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AngularAuthAPI.Context;
+using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -33,10 +36,47 @@ namespace AngularAuthAPI.Controllers
         public async Task<IActionResult> Register([FromBody] User userObj) {
             if (userObj == null) { return BadRequest(); }
 
+            // check username
+            if (await CheckUsernameExistAsync(userObj.Username)) { return BadRequest(new { Message = "Username already exists!" }); }
+
+            // check email
+            if (await CheckEmailExistAsync(userObj.Email)) { return BadRequest(new { Message = "This email is already being used!" }); }
+
+            // check password strength
+            var pass = CheckPasswordStrength(userObj.Password);
+
+            if (!string.IsNullOrEmpty(pass)) { return BadRequest(new { Message = pass.ToString() });}
+
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
+            userObj.Role = "User";
+            userObj.Token = "";
+
             await _context.Users.AddAsync(userObj);
             await _context.SaveChangesAsync();
 
             return Ok(new { Message = "User registered successfully!"});
+        }
+
+        private async Task<bool> CheckUsernameExistAsync(string username) {
+            return await _context.Users.AnyAsync(u => u.Username == username);
+        }
+
+        private async Task<bool> CheckEmailExistAsync(string email) {
+            return await _context.Users.AnyAsync(u => u.Email == email);
+        }
+
+        private string CheckPasswordStrength(string password) {
+            StringBuilder sb = new();
+
+            if (password.Length < 9) { sb.Append("Minimum password length should be 8" + Environment.NewLine); }
+
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password, "[0-9]"))) {
+                sb.Append("Password should be alphanumeric" + Environment.NewLine);
+            }
+            
+            if ((!Regex.IsMatch(password, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))) { sb.Append("Password should contain any special character" + Environment.NewLine); }
+
+            return sb.ToString();
         }
     }
 }
